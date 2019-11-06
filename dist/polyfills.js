320aa840b6666f1a68897655f72b96781eac9c66
MAbdullah078 <abdullah.khan41396@gmail.com>
2019-11-06 21:40:22 +0500

    tryr

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -5776,7 +5776,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Muhammad Usman Khan\Documents\GitHub\Influexpert_Brand\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/abdullah/Documents/GitHub/Influexpert_Brand/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
